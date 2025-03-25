8b0335da1885f63d2e0be2edef0a23f999f7e1e5
Jeremy Ip <1155176223@link.cuhk.edu.hk>
2025-03-25 16:47:47 +0800

    remove intra crossover, and mutation rate is fixed

--- job_script.sh
+++ job_script.sh
@@ -2,7 +2,7 @@
 #SBATCH --job-name=my_job     # Job name
 #SBATCH --output=output.log   # Output file
 #SBATCH --error=error.log     # Error file
-#SBATCH --partition=general   # Partition name
+#SBATCH --partition=gpu40  # Partition name
 #SBATCH --nodes=1             # Number of nodes
 #SBATCH --ntasks=1            # Number of tasks
 
