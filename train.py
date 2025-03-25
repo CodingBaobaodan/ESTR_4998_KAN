db5e72b0825f3299ec9d875d7add691eab84fcef
Jeremy Ip <1155176223@link.cuhk.edu.hk>
2025-03-25 10:27:47 +0800

    check total number of trading days only for the chromosome remain in last generation (optimal one)

--- train.py
+++ train.py
@@ -626,7 +626,7 @@ class TestLossLoggerCallback(Callback):
             self.test_losses.append(avg_loss.item())
             print(f", Average Test Loss = {avg_loss.item():.4f}")
 
-        total_testing_trading_days = trainer.callback_metrics.get('test/total_testing_trading_days')
+        total_testing_trading_days = trainer.callback_metrics.get('test/error_percentage')
         print(total_testing_trading_days)
         self.total_testing_trading_days.append(total_testing_trading_days)
 
